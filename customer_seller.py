8ef9e8ee58aa6d779b4b60bc46f5bffe6f6f4d5e
Даниил Ломов <danimbowork@gmail.com>
2023-03-31 04:00:50 +0300

    modified

--- customer_seller.py
+++ customer_seller.py
@@ -1,9 +1,11 @@
 import smtplib
-import re
+
 from openpyxl import load_workbook
 from email.mime.text import MIMEText
-import asyncio
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import os
+import mimetypes
 
 mails = []
 
@@ -21,18 +23,35 @@ for row in ws.iter_rows(min_row=5, min_col=8, max_col=8, max_row=8, values_only=
 def send_email(message, receiver):
     sender = "lomex2014@mail.ru"
     # your password = "your password"
-    password = "aPLznLee2G7TiaMLcRxf"
+    password = "x3cy6Hw2kbwC7qSp4d7y"
     # brugrmgulsghtwok
     server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
     print(receiver)
  
     try:
         server.login(sender, password)
-        msg = MIMEText(message)
-        msg["Subject"] = "Это проверочное письмо"
-        server.sendmail(sender, receiver, msg.as_string())
+        # msg = MIMEText(message)
+        msg = MIMEMultipart()
+        msg["Subject"] = "Офсетная печать по низким ценам"
+        
 
-        # server.sendmail(sender, sender, f"Subject: CLICK ME PLEASE!\n{message}")
+
+        msg.attach(MIMEText(message))
+
+        for file in os.listdir("attachments"):
+            filename = os.path.basename(file)
+            ftype, encoding = mimetypes.guess_type(file)
+            file_type, subtype = ftype.split("/")
+            print(file_type, subtype)
+
+            if file_type == "application":
+                with open(f"attachments/{file}", "rb") as f:
+                    file = MIMEApplication(f.read(), subtype)
+
+        file.add_header('content-disposition', 'attachment', filename=filename)
+        msg.attach(file)
+
+        server.sendmail(sender, receiver, msg.as_string())
 
         return "The message was sent successfully!"
     except Exception as _ex:
@@ -43,7 +62,23 @@ print(len(mails))
 
 
 for mail in mails:
-        message = "Проверочное письмо"
+        message = """Рекламное Агентство ХХI век полного цикла предлагает услуги офсетной печати визиток, листовок 
+и буклетов по низким ценам. 
+
+Мы предоставляем лучшие решения для печати, которые осуществляем по современным 
+технологиям и стандартам. 
+                     
+Разрабатываем креативный дизайн рекламной продукции.
+
+Быстрый и удобный формат заказа онлайн.
+
+Нам доверяют сотни довольных клиентов: Администрация города Мурманска,  Ростелеком, 
+КОЛАТОМЭНЕРГОСБЫТ, МТС, НМКК ФОРМАП, ГОБУ МРИБИ, Роснефть, Почта РФ, ГАЗПРОМ, ГОБУЗ 
+МОКМЦ, Хлебопек, МГТУ, ТЦ Мегастрой, ТЦ Форум, мебельный салон Лазурит, салон красоты Тай-
+Рай, ПУЛЬС и многие другие.
+
+Во вложении коммерческое предложение с полным пакетом наших услуг, а также прайсом.
+Надеемся на долгосрочное сотрудничество."""
         print(send_email(message=message, receiver=str(mail)))
         
         
